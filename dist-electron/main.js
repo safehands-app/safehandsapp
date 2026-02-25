7489d00067469f121831216539d98d98dd8f0bd2
Siji Sam <sijisam.123@gmail.com>
2026-02-25 13:04:33 +0530

    electron js changes spotted

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,24 +1,29 @@
-import { app as e, BrowserWindow as r } from "electron";
-import o from "path";
-import { fileURLToPath as a } from "url";
-const i = o.dirname(a(import.meta.url));
-function t() {
-  const n = new r({
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
+function createWindow() {
+  const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: o.join(i, "preload.mjs"),
-      contextIsolation: !0,
-      nodeIntegration: !1
+      preload: path.join(__dirname$1, "preload.mjs"),
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
-  process.env.VITE_DEV_SERVER_URL ? n.loadURL(process.env.VITE_DEV_SERVER_URL) : n.loadFile(o.join(i, "../dist/index.html"));
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(__dirname$1, "../dist/index.html"));
+  }
 }
-e.whenReady().then(() => {
-  t(), e.on("activate", () => {
-    r.getAllWindows().length === 0 && t();
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
-e.on("window-all-closed", () => {
-  process.platform !== "darwin" && e.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
--- dist-electron/preload.js
+++ dist-electron/preload.js
@@ -1,9 +1,15 @@
-import { contextBridge as a, ipcRenderer as i } from "electron";
-a.exposeInMainWorld("electron", {
-  send: (e, n) => {
-    ["toMain"].includes(e) && i.send(e, n);
+import { contextBridge, ipcRenderer } from "electron";
+contextBridge.exposeInMainWorld("electron", {
+  send: (channel, data) => {
+    let validChannels = ["toMain"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
   },
-  receive: (e, n) => {
-    ["fromMain"].includes(e) && i.on(e, (o, ...d) => n(...d));
+  receive: (channel, func) => {
+    let validChannels = ["fromMain"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
   }
 });
